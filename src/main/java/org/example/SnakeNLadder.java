78e06a5b1600ff36b93287a2111039cc1eb87e4f
Aditi Bhonle <aditirbhonsle2824@gmail.com>
2021-08-06 11:48:14 +0530

    Added comments

--- src/main/java/org/example/SnakeNLadder.java
+++ src/main/java/org/example/SnakeNLadder.java
@@ -8,6 +8,7 @@ public class SnakeNLadder
     public static void main( String[] args )
     {
         System.out.println( "Welcome to Snake and Ladder Game" );
+        //variables
         final int playerPosition=0;
         System.out.println("Player starts at Position: "+playerPosition);
     }
