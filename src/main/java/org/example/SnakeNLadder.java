ec0d5ea328830de8d1067ebfab16b8d0f671f3fd
Aditi Bhonle <aditirbhonsle2824@gmail.com>
2021-08-06 11:51:05 +0530

    Added comments

--- src/main/java/org/example/SnakeNLadder.java
+++ src/main/java/org/example/SnakeNLadder.java
@@ -1,14 +1,15 @@
 package org.example;
 
 /**
- * Hello world!
- *
+ * Snake and Ladder game where player starts at 0 position,
+ * rolls dice to get random number(1-6)
  */
 public class SnakeNLadder
 {
     public static void main( String[] args )
     {
         System.out.println( "Welcome to Snake and Ladder Game" );
+        //variables
         int playerPosition=0;
         int playerDiceNumber= ((int) (Math.floor(Math.random() * 10)) % 6) + 1;
         System.out.println("Player starts at Position: "+playerPosition);
