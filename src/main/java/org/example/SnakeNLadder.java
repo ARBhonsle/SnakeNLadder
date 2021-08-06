9d197e047eb2cd8f0ea8d5283795f09157378120
Aditi Bhonle <aditirbhonsle2824@gmail.com>
2021-08-06 07:59:27 +0530

    Added Snake&Ladder Game Project files

---
+++ src/main/java/org/example/SnakeNLadder.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class SnakeNLadder
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Snake and Ladder Game" );
+    }
+}
