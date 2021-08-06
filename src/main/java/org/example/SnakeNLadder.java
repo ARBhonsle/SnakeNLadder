22d563193d22b373f3b3b865f68fd7362d48fa7c
Aditi Bhonle <aditirbhonsle2824@gmail.com>
2021-08-06 08:55:32 +0530

    Modified player position

--- src/main/java/org/example/SnakeNLadder.java
+++ src/main/java/org/example/SnakeNLadder.java
@@ -9,7 +9,7 @@ public class SnakeNLadder
     public static void main( String[] args )
     {
         System.out.println( "Welcome to Snake and Ladder Game" );
-        final int PLAYER_START_POSITION=0;
-        System.out.println("Player starts at Position: "+PLAYER_START_POSITION);
+        final int playerPosition=0;
+        System.out.println("Player starts at Position: "+playerPosition);
     }
 }
