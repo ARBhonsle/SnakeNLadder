fe8af35f5464756e6d128ce5c8170402882098f2
Aditi Bhonle <aditirbhonsle2824@gmail.com>
2021-08-06 08:53:35 +0530

    Modified player position

--- src/main/java/org/example/SnakeNLadder.java
+++ src/main/java/org/example/SnakeNLadder.java
@@ -9,9 +9,9 @@ public class SnakeNLadder
     public static void main( String[] args )
     {
         System.out.println( "Welcome to Snake and Ladder Game" );
-        final int PLAYER_START_POSITION=0;
+        int playerPosition=0;
         int playerDiceNumber= ((int) (Math.floor(Math.random() * 10)) % 6) + 1;
-        System.out.println("Player starts at Position: "+PLAYER_START_POSITION);
+        System.out.println("Player starts at Position: "+playerPosition);
         System.out.println("Player rolled dice value : "+playerDiceNumber);
     }
 }
