c303352b1d0eda47b658262d642c7318b29468ed
Aditi Bhonle <aditirbhonsle2824@gmail.com>
2021-08-06 08:50:05 +0530

    Added player game options

--- src/main/java/org/example/SnakeNLadder.java
+++ src/main/java/org/example/SnakeNLadder.java
@@ -9,9 +9,25 @@ public class SnakeNLadder
     public static void main( String[] args )
     {
         System.out.println( "Welcome to Snake and Ladder Game" );
-        final int PLAYER_START_POSITION=0;
+        final int NO_PLAY=0,LADDER=1,SNAKE=2;
         int playerDiceNumber= ((int) (Math.floor(Math.random() * 10)) % 6) + 1;
-        System.out.println("Player starts at Position: "+PLAYER_START_POSITION);
+        int playerOption=(int) (Math.floor(Math.random() * 10)) % 3;
+        int playerPosition=0;
+        System.out.println("Player starts at Position: "+playerPosition);
         System.out.println("Player rolled dice value : "+playerDiceNumber);
+        switch (playerOption){
+            case NO_PLAY:
+                System.out.println("No Play");
+                break;
+            case LADDER:
+                System.out.println("Ladder");
+                playerPosition+=playerDiceNumber;
+                break;
+            case SNAKE:
+                System.out.println("Snake");
+                playerPosition-=playerDiceNumber;
+                break;
+        }
+        System.out.println("Player position: "+playerPosition);
     }
 }
