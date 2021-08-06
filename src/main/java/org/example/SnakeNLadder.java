f4b01429d51ba88c76bf17a3d8b04f1f2cd8d951
Aditi Bhonle <aditirbhonsle2824@gmail.com>
2021-08-06 08:38:42 +0530

    Added dice so player can roll a dice number randomly

--- src/main/java/org/example/SnakeNLadder.java
+++ src/main/java/org/example/SnakeNLadder.java
@@ -10,6 +10,8 @@ public class SnakeNLadder
     {
         System.out.println( "Welcome to Snake and Ladder Game" );
         final int PLAYER_START_POSITION=0;
+        int playerDiceNumber= ((int) (Math.floor(Math.random() * 10)) % 6) + 1;
         System.out.println("Player starts at Position: "+PLAYER_START_POSITION);
+        System.out.println("Player rolled dice value : "+playerDiceNumber);
     }
 }
